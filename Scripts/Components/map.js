79c8ae561e21ab28582d9d2ad197174e40a8f23b
Micaela <micaela.pike@gmail.com>
2022-06-24 09:46:34 +0200

    Working Icons and Popups

--- Scripts/Components/map.js
+++ Scripts/Components/map.js
@@ -1,3 +1,4 @@
+//Import JSON file with coordinates
 async function createMarker() {
 
     const { default: storeMarker } = await
@@ -10,30 +11,38 @@ async function createMarker() {
     return storeMarker;
 }
 
-let map = L.map("map").setView([-31.075604644053644, 24.43222989279748], 5);
+//Create Icon Object
+let SuperCardIcon = L.icon({
+    iconUrl: "/Assets/Logos/SuperCardFavicon.png",
+    iconSize: [20, 20],
+    iconAnchor: [10, 20],
+    popUpAnchor: [0, 0]
+})
 
+//Map View on Load
+let map = L.map("map").setView([-28.0046, 26.7732], 5);
+
+//Map Layers
 let osm = L.tileLayer("https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=jzUEBDy0K2YzaDzSHs7U", {
     attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
 });
 
 osm.addTo(map);
 
-let SuperCardIcon = L.icon({
-    iconUrl: "/Assets/Logos/SuperCardFavicon.png",
-    iconSize: [20, 20],
-    iconAnchor: [10, 10],
-})
-
 const markerLocations = await createMarker();
 
-console.log(markerLocations);
+//Marker load confirmation
+//console.log(markerLocations);
 
-// markers
-L.marker([-31.0451, 24.4411], { icon: SuperCardIcon }).addTo(map);
+//Central Marker
+// L.marker([-28.0046, 26.7732], { icon: SuperCardIcon })
+//     .bindPopup("Central Marker").addTo(map);
 
-
+//Markers
 markerLocations.forEach(function(item, index) {
     console.log(markerLocations[index].Longitude, markerLocations[index].Latitude);
-    L.marker([markerLocations[index].Longitude, markerLocations[index].Latitude], { icon: SuperCardIcon }).addTo(map);
-
-})
+    L.marker([markerLocations[index].Longitude, markerLocations[index].Latitude], { icon: SuperCardIcon })
+        .bindPopup(`${markerLocations[index].StoreName.bold()} <br>
+        ${markerLocations[index].Address}`)
+        .addTo(map);
+});
