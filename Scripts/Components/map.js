c3851f2961cc95ec7310b5c5d0eafcbd15bc8157
Micaela <micaela.pike@gmail.com>
2022-06-27 15:19:40 +0200

    Zoom onnIcon Click (incomplete)

--- Scripts/Components/map.js
+++ Scripts/Components/map.js
@@ -10,6 +10,7 @@ async function createMarker() {
 
     return storeMarker;
 }
+//debugger;
 
 //Create Icon Object
 let SuperCardIcon = L.icon({
@@ -20,16 +21,22 @@ let SuperCardIcon = L.icon({
 })
 
 //Map View on Load
-let map = L.map("map").setView([-28.0046, 26.7732], 5);
+let map = L.map('map', {
+    minZoom: 0,
+    maxZoom: 20
+});
+
+map.setView([-28.0046, 26.7732], 5);
 
 //Map Layers
 let OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
+    maxZoom: 20,
+
     // attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
 
 let Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-    maxZoom: 19,
+    maxZoom: 20,
     // attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 });
 
@@ -39,42 +46,46 @@ let mapLayer = "osMap";
 // Change Map View
 let btnLayerChange = document.querySelector("#btnLayerChange");
 
-// debugger;
-console.log(mapLayer);
-
 function changeLayer() {
 
     if (mapLayer == "osMap") {
-        Esri_WorldImagery.addTo(map)
+        OpenStreetMap_Mapnik.setZIndex(0).addTo(map);
+        Esri_WorldImagery.setZIndex(9999).addTo(map)
         mapLayer = "ewiMap";
-        console.log(mapLayer);
 
     } else if (mapLayer == "ewiMap") {
-        OpenStreetMap_Mapnik.addTo(map);
+        Esri_WorldImagery.setZIndex(0).addTo(map)
+        OpenStreetMap_Mapnik.setZIndex(9999).addTo(map);
         mapLayer = "osMap";
-        console.log(mapLayer);
-
     }
 }
 
 btnLayerChange.addEventListener("click", changeLayer);
 
-
-
 const markerLocations = await createMarker();
+//debugger;
+var markerCluster = new L.MarkerClusterGroup({
+    iconCreateFunction: function() {
+        return L.divIcon({
+            html: '<div></div>',
+            width: "20px",
+            heigh: "20px",
+            className: 'markerIcon',
+            iconSize: L.point(20, 20),
+
+        });
+    },
+});
 
 //Marker load confirmation
-//console.log(markerLocations);
-
-//Central Marker
-// L.marker([-28.0046, 26.7732], { icon: SuperCardIcon })
-//     .bindPopup("Central Marker").addTo(map);
+console.log(markerLocations);
 
 //Markers
 markerLocations.forEach(function(item, index) {
-    // console.log(markerLocations[index].Longitude, markerLocations[index].Latitude);
-    L.marker([markerLocations[index].Longitude, markerLocations[index].Latitude], { icon: SuperCardIcon })
-        .bindPopup(`${markerLocations[index].StoreName.bold()} <br>
-        ${markerLocations[index].Address}`)
-        .addTo(map);
+    console.log(markerLocations[index].Longitude, markerLocations[index].Latitude);
+    markerCluster.addLayer(L.marker([markerLocations[index].Longitude, markerLocations[index].Latitude], { icon: SuperCardIcon }).bindPopup(`${markerLocations[index].StoreName.bold()} <br>
+        ${markerLocations[index].Address}`));
+    markerCluster.addTo(map).on('click', function(e) {
+        map.panTo(e.latlng).setZoom(20);
+    });
 });
