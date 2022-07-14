efba04d78506d26e6e102332a0318e8505c33cf3
Micaela <micaela.pike@gmail.com>
2022-07-14 12:45:55 +0200

    final edits part 2

--- Scripts/Components/navigationBar.js
+++ Scripts/Components/navigationBar.js
@@ -1,43 +1,37 @@
 let menuContainer = document.querySelector(".burger");
 let hamburgerNavList = document.querySelector(".hamburgerNavList");
+let heightOutput = window.innerHeight;
+let widthOutput = window.innerWidth;
 
 function changeHamburger(item) {
     console.log(item.classList);
     item.classList.toggle("change");
 }
 
-// function removeChange(element) {
-//     if (element.classList.contains("change")) {
-//         element.classList.remove("change");
-//         element.offsetWidth;
-//     }
+function reportWindowSize() {
+    heightOutput = window.innerHeight;
+    widthOutput = window.innerWidth;
 
-//     // element.classList.toggle("change");
+    if (widthOutput >= 880) {
+        debugger
+        removeChange(menuContainer);
+        removeChange(hamburgerNavList);
+    }
+}
 
-// }
+window.onresize = reportWindowSize;
 
-// let mediaQuery = window.matchMedia("(min-width: 880px)");
-// // removeChange(menuContainer); // Call listener function at run time
-// // removeChange(hamburgerNavList); // Call listener function at run time
-// menuContainer.addEventListener("resize", mediaQuery); // Attach listener function on state changes
-// hamburgerNavList.addListener("resize", mediaQuery); // Attach listener function on state changes
+function removeChange(element) {
+    if (element.classList.contains("change")) {
+        element.classList.remove("change");
+        element.offsetWidth;
+    }
+}
 
-// if (mediaQuery.matches) {
-//     removeChange(menuContainer);
-//     removeChange(hamburgerNavList);
-// }
+
+
 
 menuContainer.addEventListener("click", () => {
     changeHamburger(menuContainer);
     changeHamburger(hamburgerNavList);
 });
-
-
-
-// if (window.matchMedia("(max-width: 880px)").matches) {
-//     if (hamburgerNavList.classList.contains("change")) {
-//         hamburgerNavList.classList.remove("change");
-//         hamburgerNavList.offsetWidth;
-
-//     }
-// }
