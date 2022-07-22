35693ab0f79d1b187c937203eefd2994744f08a7
Micaela <micaela.pike@gmail.com>
2022-07-22 08:21:34 +0200

    NavBar Final

--- Scripts/Components/navigationBar.js
+++ Scripts/Components/navigationBar.js
@@ -1,15 +1,12 @@
 let menuContainer = document.querySelector(".burger");
-let navList = document.querySelector(".navList");
 let hamburgerNavList = document.querySelector(".hamburgerNavList");
 let heightOutput = window.innerHeight;
 let widthOutput = window.innerWidth;
 
+
+
 function changeHamburger(item) {
     item.classList.toggle("change");
-}
-
-function changeNavList(item) {
-    item.classList.toggle("mobile")
 }
 
 function reportWindowSize() {
@@ -20,20 +17,14 @@ function reportWindowSize() {
         removeChange(menuContainer);
         removeChange(hamburgerNavList);
     }
-
-    if (widthOutput <= 880) {
-        changeNavList(navList);
-    }
 }
-
-window.onresize = reportWindowSize;
 
 function removeChange(element) {
     if (element.classList.contains("change")) {
         element.classList.remove("change");
-        element.offsetWidth;
     }
 }
+window.onresize = reportWindowSize;
 
 menuContainer.addEventListener("click", () => {
     changeHamburger(menuContainer);
